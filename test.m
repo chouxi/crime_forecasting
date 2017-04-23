15904366ef169f8c9063db4a5bddf50ab0a29cd0
klory <hfdtsinghua@gmail.com>
2017-04-23 18:53:58 -0400

    add .gif and .DS_Store into ignore

--- test.m
+++ test.m
@@ -5,5 +5,5 @@ clc
 [X,Y,Census,T] = preprocess('TOA');
 
 %% gennerate heat maps
-[data, countMaps, censusMaps] = generateByPeriodAndGrid('SC', '1MO', 600);
+[data, countMaps, censusMaps] = generateByPeriodAndGrid('BURG', '1MO', 600);
 bar(1:length(data.summary), data.summary);
