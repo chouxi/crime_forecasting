024a8528ea6adfd221fa2e94c0c628162a30aab2
klory <hfdtsinghua@gmail.com>
2017-04-23 20:05:04 -0400

    add computeResultRange.m

--- test.m
+++ test.m
@@ -9,7 +9,12 @@ category = 'SC';
 period = '1MO';
 [data, countMaps, censusMaps] = generateByPeriodAndGrid(category,period,600);
 
-%%
+%% draw data (total)
 bar(1:length(data.summary), data.summary);
 title([category,'-',period]);
+
+%% save gifs
 % saveGifs(category,period,countMaps,censusMaps);
+
+%% compute the range of number of hotspot
+[nRange, nTotal] = computeResultRange(600);
